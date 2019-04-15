99998b991c9b4ab6ca48a1f7f3ec4e3c7d50e759
Konstantin Sharohin <ksharohin@gmail.com>
2019-04-15 12:01:31 +0300

    Cart markup changed in resp. table

--- cart.php
+++ cart.php
@@ -10,21 +10,78 @@
 	<h1 class="cart-page-main-title">Корзина</h1>
 	<div class="cart-page-inner-container">
 		<h2 class="cart-page-title">Выбраные конструкторы</h2>
-		<div class="cart-page">
-			<div class="cart-page-row photo">
+		<div class="container-table">
+			<div class="table">
+				<div class="table-row header">
+					<div class="cell">
+						Наименование
+					</div>
+					<div class="cell">
+						Количество (шт.)
+					</div>
+					<div class="cell">
+						Цена (за шт.)
+					</div>
+				</div>
+
+				<div class="table-row">
+					<div class="cell" data-title="Наименование">
+						Ninja Saga
+					</div>
+					<div class="cell" data-title="Количество (шт.)">
+						1
+					</div>
+					<div class="cell" data-title="Цена (за шт.)">
+						200 грн
+					</div>
+				</div>
+
+				<div class="table-row">
+					<div class="cell" data-title="Наименование">
+						Ninja 2
+					</div>
+					<div class="cell" data-title="Количество (шт.)">
+						1
+					</div>
+					<div class="cell" data-title="Цена (за шт.)">
+						300 грн
+					</div>
+				</div>
+			</div>
+
+			<div class="table total">
+				<div class="table-row header total">
+					<div class="cell">
+						Итого
+					</div>
+				</div>
+
+				<div class="table-row total">
+					<div class="cell" data-title="Итого">
+						500 грн
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- <div class="cart-page">
+			<div class="cart-page-row-titles">
 				<div class="cart-page-description-container">
 					<div class="cart-page-description">Наименование</div>
 				</div>
-			</div>
-			<div class="cart-page-column quantity">
 				<div class="cart-page-description-container">
 					<div class="cart-page-description">Количество (шт.)</div>
 				</div>
 			</div>
+
+			<div class="cart-page-column quantity">
+
+			</div>
 			<div class="cart-page-column price">
-				<div class="cart-page-description-container">
-					<div class="cart-page-description">Цена за единицу</div>
-				</div>
+
 			</div>
 			<div class="cart-page-column total">
 				<div class="cart-page-description-container">
@@ -33,7 +90,7 @@
 			</div>
 		</div>
 		<button type="submit" class="cart-submit-btn">Оформить заказ</button>
-	</div>
+	</div> -->
 	<?php
 		echo '<button class="return-to-top-btn" title="Вернуться наверх">
 					<span class="up-symbol">
